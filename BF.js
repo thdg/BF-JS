b05929715d2fe1d1aee1b8aa3828d19047c3542e
thorsteinn <thdg90@gmail.com>
2013-09-01 15:50:27 +0000

    All but loops

--- BF.js
+++ BF.js
@@ -8,30 +8,29 @@ Tape.prototype.P = function(val) { return this.tape[this.head] = val; }
 // F = TF | '[',F,']'
 // T = '+','-','<','>','.',','
 
-var run = function(program) { 
+var db = function(val) { console.log(val); }
+
+var run = function(program, size) { 
+    var memory = new Array(size); while(size--) { memory[size]=0; }
+    var memory_tape = new Tape(memory);
     var program_tape = new Tape(program); 
-    var memory_tape = new Tape(new Array()); 
-    while (program_tape.R()) { F(); } 
-}
 
-var F = function() {
-    if (program_tape.G()!="]") { T(); }
-    else if (memory_tape!=0) { 
-        var skip = 0; 
-        while (program_tape.G()!='[' || skip!=0) { program_tape.L(); } } 
-    else { program_tape.R(); }
-    F();
-}
-
-var T = function() {
-    switch (program_tape.G()) {
-        case '+': memory_tape.P(++memory_tape.G()); break;
-        case '-': memory_tape.P(--memory_tape.G()); break;
-        case '.': console.log(memory_tape.G()); break;
-        case ',': break;
-        case '<': memory_tape.L(); break;
-        case '>': memory_tape.R(); break;
+    var T = function() {
+        switch (program_tape.G()) {
+            case '+': memory_tape.P(memory_tape.G()+1); break;
+            case '-': memory_tape.P(memory_tape.G()-1); break;
+            case '<': memory_tape.L(); break;
+            case '>': memory_tape.R(); break;
+            case '.': console.log(memory_tape.G()); break;
+            case ',': memory_tape.P(100); break;
+        }
     }
-    program_tape.R();
+
+    var F = function() {
+        if (program_tape.G()=="[") { program_tape.R(); F(); } else
+        if (program_tape.G()!="]") { T(); }
+        program_tape.R();
+    }
+
+    while (program_tape.G()) { F(); } 
 }
-        
