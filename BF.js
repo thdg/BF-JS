0e69d252ad5a4fc89628876a81fac89e9d054135
Þorsteinn Daði Gunnarsson <thdg90@gmail.com>
2013-09-03 23:35:16 +0000

    Yayyy it works!

--- BF.js
+++ BF.js
@@ -3,18 +3,13 @@ Tape.prototype.L = function() { return --this.head; }
 Tape.prototype.R = function() { return ++this.head; }
 Tape.prototype.G = function() { return this.tape[this.head]; }
 Tape.prototype.P = function(val) { return this.tape[this.head] = val; }
+Tape.prototype.J = function(cell) { return this.head = cell; }
 
-// EBNF
-// F = TF | '[',F,']'
-// T = '+', '-', '<', '>', '.', ','
-
-var db = function(val) { console.log(val); }
-
-var run = function(program, size) { 
+var run = function(program, size, input) { 
     var memory = new Array(size); while(size--) { memory[size]=0; }
     var memory_tape = new Tape(memory);
-    var program_tape = new Tape(program); 
-
+    var program_tape = new Tape(program);
+    var input_tape = new Tape(input); 
     var T = function() {
         switch (program_tape.G()) {
             case '+': memory_tape.P(memory_tape.G()+1); break;
@@ -22,22 +17,22 @@ var run = function(program, size) {
             case '<': memory_tape.L(); break;
             case '>': memory_tape.R(); break;
             case '.': console.log(String.fromCharCode(memory_tape.G())); break;
-            case ',': memory_tape.P(100); break;
+            case ',': memory_tape.P(input_tape.G().charCodeAt()); input_tape.R(); break;
+            case ']': console.log('Unexpected bracket "]"'); break;
+            case '[': var start = program_tape.head;
+                      while (memory_tape.G()!=0) { 
+                          program_tape.R();
+                          while ( program_tape.G()!=']' ) { T(); }
+                          program_tape.J(start);
+                      }
+                      var skip = 1;
+                      while (skip>0) { 
+                        program_tape.R(); 
+                        if (program_tape.G()=='[') { skip++; }
+                        if (program_tape.G()==']') { skip--; }
+                      }
         }
+        program_tape.R();
     }
-
-    var F = function() {
-        db(program_tape.G());
-        if (program_tape.G()=="[") { 
-            program_tape.R(); 
-            while (program_tape.G()!=']') { F(); } 
-            if (memory_tape.G()!=0) {
-                while (program_tape.G()!='[') {
-                    program_tape.L();
-                }
-            }
-        } else { T(); program_tape.R(); }
-    }
-
-    while (program_tape.G()) { F(); } 
+    while (program_tape.G()) { T(); } 
 }
