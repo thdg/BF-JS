9b764148e973f48f55e9b2fa5aae17ddbd54053e
thorsteinn <thdg90@gmail.com>
2013-09-21 20:39:16 +0000

    bug fix

--- BF.js
+++ BF.js
@@ -12,29 +12,32 @@ function I() {
     case '<': memory_tape.L(); break;
     case '>': memory_tape.R(); break;
     case '.': output += String.fromCharCode(memory_tape.G()); break;
-    case ',': input_tape.G()!==undefined ? memory_tape.P(input_tape.G().charCodeAt()) : memory_tape.P(0); input_tape.R(); break;
+    case ',': memory_tape.P(input_tape.G().charCodeAt()); input_tape.R(); break;
     case ']': output = 'Unexpected bracket "]"'; program_tape.J(program_tape.tape.length); return;
-    case '[': var start = program_tape.head, skip=1;
+    case '[': start.push(program_tape.head);
               while (memory_tape.G()>0) { 
                 program_tape.R();
                 while ( program_tape.G()!=']' ) { I(); }
-                program_tape.J(start);
+                program_tape.J(start[start.length-1]);
               }
+              start.pop();
+              var skip = 1;
               while (skip>0) { 
                 program_tape.R(); 
                 if (program_tape.G()==='[') { skip++; }
                 if (program_tape.G()===']') { skip--; }
               }
   }
-  program_tape.R(); console.log(output);
+  program_tape.R();
+
 }
 
 function bf(program, size, input) { 
   var memory = new Array(size); while(size--) { memory[size]=0; }
       memory_tape = new Tape(memory),
       program_tape = new Tape(program),
-      input_tape = new Tape(input),
-      output = '';
+      input_tape = new Tape(input+String.fromCharCode(undefined)),
+      output = '', start = [];
   while (program_tape.G()) { I(); }
   return output;
 }
--- adapter.js
+++ adapter.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
 			input = $('#input', controler).val(),
 			program = $('#program', controler).val(),
 			memory = $('#memory', controler).val();
+			if (memory==='') { memory = 30000; }
 		$('#output', controler).val(bf(program, memory, input));
 	}
 	$('#run').click(run);
