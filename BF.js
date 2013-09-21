cec6fa693c595f4f9bc5bbc1dae439a181286e23
Þorsteinn Daði Gunnarsson <thdg90@gmail.com>
2013-09-21 00:06:28 +0000

    Readme update and bf cleanup

--- BF.js
+++ BF.js
@@ -5,34 +5,36 @@ Tape.prototype.G = function() { return this.tape[this.head]; }
 Tape.prototype.P = function(val) { return this.tape[this.head] = val; }
 Tape.prototype.J = function(cell) { return this.head = cell; }
 
-var run = function(program, size, input) { 
-    var memory = new Array(size); while(size--) { memory[size]=0; }
-    var memory_tape = new Tape(memory);
-    var program_tape = new Tape(program);
-    var input_tape = new Tape(input); 
-    var T = function() {
-        switch (program_tape.G()) {
-            case '+': memory_tape.P(memory_tape.G()+1); break;
-            case '-': memory_tape.P(memory_tape.G()-1); break;
-            case '<': memory_tape.L(); break;
-            case '>': memory_tape.R(); break;
-            case '.': console.log(String.fromCharCode(memory_tape.G())); break;
-            case ',': memory_tape.P(input_tape.G().charCodeAt()); input_tape.R(); break;
-            case ']': console.log('Unexpected bracket "]"'); break;
-            case '[': var start = program_tape.head;
-                      while (memory_tape.G()!=0) { 
-                          program_tape.R();
-                          while ( program_tape.G()!=']' ) { T(); }
-                          program_tape.J(start);
-                      }
-                      var skip = 1;
-                      while (skip>0) { 
-                        program_tape.R(); 
-                        if (program_tape.G()=='[') { skip++; }
-                        if (program_tape.G()==']') { skip--; }
-                      }
-        }
-        program_tape.R();
-    }
-    while (program_tape.G()) { T(); } 
+function I() {
+  switch (program_tape.G()) {
+    case '+': memory_tape.P(memory_tape.G()+1); break;
+    case '-': memory_tape.P(memory_tape.G()-1); break;
+    case '<': memory_tape.L(); break;
+    case '>': memory_tape.R(); break;
+    case '.': output += String.fromCharCode(memory_tape.G()); break;
+    case ',': memory_tape.P(input_tape.G().charCodeAt()); input_tape.R(); break;
+    case ']': return 'Unexpected bracket "]"';
+    case '[': var start = program_tape.head, skip=1;
+              while (memory_tape.G()!=0) { 
+                program_tape.R();
+                while ( program_tape.G()!=']' ) { I(); }
+                program_tape.J(start);
+              }
+              while (skip>0) { 
+                program_tape.R(); 
+                if (program_tape.G()=='[') { skip++; }
+                if (program_tape.G()==']') { skip--; }
+              }
+  }
+  program_tape.R();
+}
+
+function bf(program, size, input) { 
+  var memory = new Array(size); while(size--) { memory[size]=0; }
+      memory_tape = new Tape(memory),
+      program_tape = new Tape(program),
+      input_tape = new Tape(input),
+      output = '';
+  while (program_tape.G()) { I(); }
+  return output;
 }
--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 BF-JS v0.1
 =====
 
-Brainfuck interpreter in JavaScript
+Javascript Brainfuck interpreter in 40 lines.
+
+Usage:
+
+  // before:
+  //   program -> program as string
+  //   size    -> size of memory tape
+  //   input   -> input as string
+  // after:
+  //   output  -> program output as string
+  output = bf(program,size,input);
